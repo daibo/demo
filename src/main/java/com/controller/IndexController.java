bc88b4b10c8b96f2831f2a1fb9a80bb9d73f26f6
dai.bo <daibo.vip@outlook.com>
2017-07-21 17:46:30 +0800

    addSomething

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,13 +1,14 @@
 package com.controller;
 
+import com.Pojo.User;
 import com.Pojo.UserPojo;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.logging.Logger;
 
 /**
  * Created by daibo on 2017/7/21.
@@ -16,13 +17,18 @@ import java.util.logging.Logger;
 @EnableAutoConfiguration
 public class IndexController {
     private org.slf4j.Logger log = LoggerFactory.getLogger("this.class");
-    @Autowired
-    private UserPojo user;
+
 
     @RequestMapping(value = "index")
     public String Index() {
         log.info("你进入的index控制器");
         log.warn("测试日志进行本地存储");
         return "Hello";
+    }
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String hello( ) {
+
+        return "hello";
     }
 }
